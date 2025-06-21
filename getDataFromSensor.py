a88e3f34aa95ec047207f27832837ec315d14a83
mfkoerner <mfkoerner@gmail.com>
2025-06-20 17:49:10 -0700

    Create getDataFromSensor

--- getDataFromSensor.py
+++ getDataFromSensor.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
 # Provides libraries to contact the the AirGradient sensors and return current data
+
+class airsensor():
+   """
+   Provides various getData functions for a specific sensor
+   """
+
+   def __init__(  *, hwid, name, indoor=True ):
+      self.hwid = hwid
+      self.name = name
+      self.indoor = indoor
